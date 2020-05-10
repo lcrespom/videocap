fe7a67a8a1ea2c3752c12a991e8d3d40558ad4e6
Luis Crespo <lcrespom@gmail.com>
2020-05-10 22:47:53 +0200

    Drawing all body segments

--- vcap.js
+++ vcap.js
@@ -34,9 +34,9 @@ function startWebcam() {
 }
 
 async function captureVideo(w, h) {
-    //ctx.clearRect(0, 0, WIDTH, HEIGHT)
-    //drawVideo()
     let pose = await net.estimateSinglePose(video, { flipHorizontal: true })
+    ctx.clearRect(0, 0, WIDTH, HEIGHT)
+    drawVideo()
     drawPose(pose.keypoints, ctx)
     requestAnimationFrame(() => captureVideo(w, h))
 }
@@ -56,9 +56,13 @@ function keypoints2map(keypoints) {
     return kpMap
 }
 
-function drawSegment(ctx, kpMap, from, to) {
-    let {x: x1, y: y1} = kpMap[from].position
-    let {x: x2, y: y2} = kpMap[to].position
+function drawSegment({ ctx, kpMap, minConfidence }, from, to) {
+    let kpFrom = kpMap[from]
+    let kpTo = kpMap[to]
+    if (kpFrom.score < minConfidence || kpTo.score < minConfidence)
+        return
+    let {x: x1, y: y1} = kpFrom.position
+    let {x: x2, y: y2} = kpTo.position
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
 }
@@ -73,13 +77,25 @@ function drawPose(keypoints, ctx, minConfidence = 0.5, scale = 1) {
     ctx.strokeStyle = 'cyan'
     ctx.lineWidth = 2
     ctx.beginPath()
-    drawSegment(ctx, kpMap, 'leftShoulder', 'rightShoulder')
-    drawSegment(ctx, kpMap, 'leftShoulder', 'leftHip')
-    drawSegment(ctx, kpMap, 'rightShoulder', 'rightHip')
-    drawSegment(ctx, kpMap, 'leftHip', 'rightHip')
+    // Torso
+    let drawInfo = { ctx, kpMap, minConfidence }
+    drawSegment(drawInfo, 'leftShoulder', 'rightShoulder')
+    drawSegment(drawInfo, 'leftShoulder', 'leftHip')
+    drawSegment(drawInfo, 'rightShoulder', 'rightHip')
+    drawSegment(drawInfo, 'leftHip', 'rightHip')
+    // Arms
+    drawSegment(drawInfo, 'leftShoulder', 'leftElbow')
+    drawSegment(drawInfo, 'leftElbow', 'leftWrist')
+    drawSegment(drawInfo, 'rightShoulder', 'rightElbow')
+    drawSegment(drawInfo, 'rightElbow', 'rightWrist')
+    // Legs
+    drawSegment(drawInfo, 'leftHip', 'leftKnee')
+    drawSegment(drawInfo, 'leftKnee', 'leftAnkle')
+    drawSegment(drawInfo, 'rightHip', 'rightKnee')
+    drawSegment(drawInfo, 'rightKnee', 'rightAnkle')
+    // Draw it
     ctx.stroke()
     ctx.restore()
-    console.log(kpMap.leftShoulder.position)
 }
 
 
@@ -91,7 +107,6 @@ async function main() {
         multiplier: 0.5,
         quantBytes: 2
     })
-    console.dir(net)
     startWebcam()
 }
 
